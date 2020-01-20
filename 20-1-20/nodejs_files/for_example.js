fce8b59b5f0c13890ffb0f206fac271e4967a2e3
vaishnavi-bale <59390707+vaishnavi-bale@users.noreply.github.com>
2020-01-20 17:44:18 +0530

    bootstrap cont.

---
+++ 20-1-20/nodejs_files/for_example.js
@@ -0,0 +1,7 @@
+var obj = {a:1, b:2, c:3};  
+for (var prop in obj) { 
+   console.log(obj[prop]); 
+}
+for (let val of[12 , 13 , 123]) {   
+    console.log(val) 
+ } 
---
+++ 20-1-20/nodejs_files/test.js
@@ -0,0 +1,15 @@
+test()
+let name='amit'
+const name1='rahul'
+function test(){
+    for(var index=0;index<5;index++){
+        console.log('Index : '+index)
+    }
+    var name='vaish'
+    console.log('Value of Index : '+index);
+    console.log('Name : '+name)
+    
+}
+console.log('Value of name1 : '+name1);
+
+console.log('Name : '+name)
